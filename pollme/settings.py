72df31505d4eac6987d39b240386add418ed3389
Satvik Somvanshi <satviksomvanshi@gmail.com>
2024-08-01 02:42:36 +0530

    updating docker file

--- pollme/settings.py
+++ pollme/settings.py
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'pollme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/app/db.sqlite3',  # Path to SQLite file in the container
     }
 }
 
